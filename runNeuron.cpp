9b156202b6c9fbcd86202ee4350faa792c289e5e
Quint Hester <rahv7245@mit.edu>
2024-03-03 17:29:54 -0500

    switch to direct list initialization

--- runNeuron.cpp
+++ runNeuron.cpp
@@ -20,7 +20,7 @@ void testEigenVector() {
     m(2,0) = 3;
     std::cout << m << '\n';
 
-    Matrix<double, 3, 1> M(1, 2, 3);
+    Matrix<double, 3, 1> M{1, 2, 3};
     std::cout << M << '\n';
 
     Matrix<double, 3, 1> mm = {1, 2, 3};
@@ -32,7 +32,7 @@ void testEigenVector() {
     v(2) = 3;
     std::cout << v << '\n';
 
-    Vector<double, 3> V(1, 2, 3);
+    Vector<double, 3> V{1, 2, 3};
     std::cout << V << '\n';
 }
 
@@ -69,8 +69,6 @@ void printElapsedTime(auto& start, std::string_view functionName) {
 int main()
 {   /*
     todo
-    - add gtest
-    ---
     - templatify dotProductLayer and matrixMultiplicationLayer since those may actually be reused
     - more tests with different matrix and vector sizes
     ---
@@ -82,19 +80,19 @@ int main()
 
     // these functions could've been done equivalently with std::vector
 
-    Vector<double, 4> inputs(1.0, 2.0, 3.0, 2.5);
-    Vector<double, 4> weights(0.2, 0.8, -0.5, 1.0);
-    double bias = 2.0;
+    Vector<double, 4> inputs{1.0, 2.0, 3.0, 2.5};
+    Vector<double, 4> weights{0.2, 0.8, -0.5, 1.0};
+    double bias{2.0};
 
     double manualNeuronOutput{manualNeuron(inputs, weights, bias)};
     std::cout << manualNeuronOutput << '\n';
 
-    Vector<double, 4> weights1(0.2, 0.8, -0.5, 1.0);
-    Vector<double, 4> weights2(0.5, -0.91, 0.26, -0.5);
-    Vector<double, 4> weights3(-0.26, -0.27, 0.17, 0.87);
-    double bias1 = 2.0;
-    double bias2 = 3.0;
-    double bias3 = 0.5;
+    Vector<double, 4> weights1{0.2, 0.8, -0.5, 1.0};
+    Vector<double, 4> weights2{0.5, -0.91, 0.26, -0.5};
+    Vector<double, 4> weights3{-0.26, -0.27, 0.17, 0.87};
+    double bias1{2.0};
+    double bias2{3.0};
+    double bias3{0.5};
 
     Vector<double, 3> manualLayerOutput{manualLayer(inputs, weights1, weights2, weights3, bias1, bias2, bias3)};
     std::cout << manualLayerOutput << '\n';
@@ -102,7 +100,7 @@ int main()
     Matrix<double, 3, 4> weights2D {{0.2, 0.8, -0.5, 1.0},
                                   {0.5, -0.91, 0.26, -0.5},
                                   {-0.26, -0.27, 0.17, 0.87}};
-    Vector<double, 3> biases(2, 3, 0.5);
+    Vector<double, 3> biases{2, 3, 0.5};
 
     Vector<double, 3> forLoopLayerOutput{forLoopLayer(inputs, weights2D, biases)};
     std::cout << forLoopLayerOutput << '\n';
